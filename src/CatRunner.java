6fe1214864b3d3be26b519e8dd3a243f854e8455
William <williamh29@nycstudents.net>
2023-01-31 23:08:24 -0500

    initialize

--- src/CatRunner.java
+++ src/CatRunner.java
@@ -10,5 +10,18 @@ public class CatRunner {
         cats.add(cat3);
         System.out.println(cats);
         Cat cat4 = new Cat("Nemo");
+        Cat swapCat = cats.set(2, cat4);
+        System.out.println(cats);
+        System.out.println(cat4);
+        cat4.setName(cat4.getName() + " Meow");
+        System.out.println(cat4);
+        System.out.println(cats);
+        for (int i = 0; i < cats.size(); i++) {
+            String capName = cats.get(i).getName().toUpperCase();
+            cats.get(i).setName(capName);
+        }
+        System.out.println(cats);
+        cats.add(new Cat("SAM"));
+        System.out.println(cats);
     }
 }
---
+++ src/UsedCarLot.java
@@ -0,0 +1,27 @@
+import java.util.ArrayList;
+public class UsedCarLot {
+    private ArrayList<Car> inventory;
+
+    public UsedCarLot() {
+        inventory = new ArrayList<Car>();
+    }
+    public ArrayList<Car> getInventory() {
+        return inventory;
+    }
+    public void addCar(Car addCar) {
+        inventory.add(addCar);
+    }
+    public boolean swap(int idx1, int idx2) {
+        if (idx1 < inventory.size() && idx2 < inventory.size()) {
+            Car car2 = inventory.get(idx2);
+            Car car1 = inventory.get(idx1);
+            inventory.set(idx2, car1);
+            inventory.set(idx1, car2);
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+
+}
